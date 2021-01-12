b1a99bc9e41233fe85cdc0f65059877864a04cbd
lakefu <lakefu@tencent.com>
2021-01-13 01:23:49 +0800

    单元测试修复

--- src/mq_test.go
+++ src/mq_test.go
@@ -9,14 +9,14 @@ import (
 func TestMsgQueue(t *testing.T) {
 	mq := NewMQueue(3)
 	data := []*Message{
-		{0, MSG_TYPE_LOGIC, 1, nil},
-		{0, MSG_TYPE_LOGIC, 2, nil},
-		{0, MSG_TYPE_LOGIC, 3, nil},
-		{0, MSG_TYPE_LOGIC, 4, nil},
-		{0, MSG_TYPE_LOGIC, 5, nil},
-		{0, MSG_TYPE_LOGIC, 6, nil},
-		{0, MSG_TYPE_LOGIC, 7, nil},
-		{0, MSG_TYPE_LOGIC, 8, nil},
+		{0, MSG_TYPE_TIMER, 1, nil},
+		{0, MSG_TYPE_TIMER, 2, nil},
+		{0, MSG_TYPE_TIMER, 3, nil},
+		{0, MSG_TYPE_TIMER, 4, nil},
+		{0, MSG_TYPE_TIMER, 5, nil},
+		{0, MSG_TYPE_TIMER, 6, nil},
+		{0, MSG_TYPE_TIMER, 7, nil},
+		{0, MSG_TYPE_TIMER, 8, nil},
 	}
 	for i := range data {
 		mq.Push(data[i])
@@ -33,13 +33,13 @@ func TestMsgQueue(t *testing.T) {
 	assert.Equal(t, 8, mq.tail)
 	assert.Equal(t, 4, mq.Len())
 	data = []*Message{
-		{0, MSG_TYPE_LOGIC, 9, nil},
-		{0, MSG_TYPE_LOGIC, 10, nil},
-		{0, MSG_TYPE_LOGIC, 11, nil},
-		{0, MSG_TYPE_LOGIC, 12, nil},
-		{0, MSG_TYPE_LOGIC, 13, nil},
-		{0, MSG_TYPE_LOGIC, 14, nil},
-		{0, MSG_TYPE_LOGIC, 15, nil},
+		{0, MSG_TYPE_TIMER, 9, nil},
+		{0, MSG_TYPE_TIMER, 10, nil},
+		{0, MSG_TYPE_TIMER, 11, nil},
+		{0, MSG_TYPE_TIMER, 12, nil},
+		{0, MSG_TYPE_TIMER, 13, nil},
+		{0, MSG_TYPE_TIMER, 14, nil},
+		{0, MSG_TYPE_TIMER, 15, nil},
 	}
 	for i := range data {
 		mq.Push(data[i])
@@ -48,14 +48,14 @@ func TestMsgQueue(t *testing.T) {
 	assert.Equal(t, 3, mq.tail)
 	assert.Equal(t, 11, mq.Len())
 	assert.Equal(t, 12, mq.cap)
-	assert.Equal(t, 5, mq.Peek().session)
+	assert.Equal(t, uint32(5), mq.Peek().session)
 	// 打印一下
 	mq.debug()
 	// 临界触发expand
-	mq.Push(&Message{0, MSG_TYPE_LOGIC, 16, nil})
+	mq.Push(&Message{0, MSG_TYPE_TIMER, 16, nil})
 	assert.Equal(t, 0, mq.head)
 	assert.Equal(t, 12, mq.tail)
 	assert.Equal(t, 12, mq.Len())
 	assert.Equal(t, 24, mq.cap)
-	assert.Equal(t, 5, mq.Peek().session)
+	assert.Equal(t, uint32(5), mq.Peek().session)
 }
